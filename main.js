de5bd595688dd3cf28b638c8d450d864d9cc52f9
sabina-sudo <sabrine.bouchiha@gmail.com>
2024-07-29 15:16:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+let tabObjet=[
+    {
+        "src": "./img/abricot.jpg",
+        "title": "Abricot",
+        "prix": "3 DT"
+      },
+      {
+        "src": "./img/amande.jpg",
+        "title": "amande",
+        "prix": "5.5 DT"
+      },
+      {
+        "src": "./img/fraise.jpg",
+        "title": "fraise",
+        "prix": "6 DT"
+      },
+      {
+        "src": "./img/jus-grenade.jpg",
+        "title": "fraise",
+        "prix": "6 DT"
+      },
+      {
+        "src": "./img/pomme.jpg",
+        "title": "fraise",
+        "prix": "6 DT"
+      },
+      {
+        "src": "./img/orange.jpg",
+        "title": "fraise",
+        "prix": "6 DT"
+      },
+      {
+        "src": "./img/jus-abricor-poire.jpg",
+        "title": "fraise",
+        "prix": "6 DT"
+      },
+]
+
+
+/* <div>
+          <img src="./img/abricot.jpg" />
+          <p>abricot </p>
+          <p>5 DT</p>
+          <a type="submit">Ajouter au panier</a>
+        </div> */
+let section = document.querySelector("section")
+let div1 = document.createElement("div");
+let image=document.createElement("img");
+let nomFruit=document.createElement("p");
+let prix = document.createElement("p");
+let 
+
+
+
+
+section.appendChild(div1);
+
+
+
+
+// console.log(div1);
